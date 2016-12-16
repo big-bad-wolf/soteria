87137155c7b1e93be829b892cedb5c7ec772ee3e
Juan E. Maya <jmayaalv@gmail.com>
2016-12-16 16:29:43 +0100

    core.clj

---
+++ src/soteria/core.clj
@@ -0,0 +1,8 @@
+(ns soteria.core
+  (:require
+   [arachne.core.config :as cfg]
+   [arachne.core.config.ontology :as o]
+   [arachne.core.config.init :as dsl]
+   [arachne.core.dsl.specs :as dslspecs]
+   [com.stuartsierra.component :as c]
+   [clojure.spec :as s]))
